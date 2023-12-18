838e566d8e8f5e7215002f0c906f7e4cc48a980c
Stephan Schöpe <socialcrap+github@posteo.de>
2023-12-18 21:49:04 +0100

    day 12 refactored

--- 12/a.ts
+++ 12/a.ts
@@ -1,37 +1,69 @@
 import input from "./input.txt";
+const records = input.split("\n");
 
-const records = input.split("\n").map((set) => set.split(" "));
-const extendedRecors = records.map((record) => {});
+const cache = new Map<string, number>();
 
-// console.log("🚀 ~ records:", records);
+function count(springs: string, check: number[]) {
+  // If both the springs and checks are empty it must be a valid solution => 1
+  // If there are no more springs in the configuration but still checks left, it can't be a valid solution => 0
+  if (springs === "") return check.length === 0 ? 1 : 0;
 
-function isValid(conditions: string, check: string) {
-  if (conditions.includes("?")) return false;
-  const foo = conditions
-    .split(".")
-    .map((set) => set.length)
-    .filter((set) => set > 0)
-    .join(",");
+  // If the checks are empty and there are no more springs left, it must be a valid solution => 1
+  // If the checks are empty but there is still a spring(#) in the configuration it cant be a valid solution => 0
+  if (check.length === 0) return springs.includes("#") ? 0 : 1;
 
-  return foo == check;
+  const key = springs + check.join(",");
+  const cached = cache.get(key);
+
+  if (cached !== undefined) return cached;
+
+  let result = 0;
+
+  // If the first item in the config is a "." or "?" call the function recursively with the first item removed
+  // This is the same as replacing the "?" with a ".".
+  if (springs[0] === "." || springs[0] === "?")
+    result += count(springs.slice(1), check);
+
+  if (springs[0] === "#" || springs[0] === "?") {
+    if (
+      // There must be at least as many springs left as the check would require
+      check[0] <= springs.length &&
+      // The consecutive spring items cannot contain a "." in the range of the check sum
+      !springs.slice(0, check[0]).includes(".") &&
+      // The check sum must be equal to the number of springs
+      // Or the spring item at the position of the check is not a "#"
+      (check[0] === springs.length || springs[check[0]] !== "#")
+    ) {
+      result += count(springs.slice(check[0] + 1), check.slice(1));
+    }
+  }
+
+  cache.set(key, result);
+  return result;
 }
 
-function bar(record: string[]) {
-  let arrangements = 0;
-  const options = ["#", "."];
-  const [input, check] = record;
-  if (!input.includes("?")) return arrangements;
-  // console.log("🚀 ~ input:", input);
+// part 1
+const part1 = records
+  .map((record) => {
+    let [springs, check] = record.split(" ");
+    let checks = check.split(",").map(Number);
+    return count(springs, checks);
+  })
+  .reduce((a, b) => a + b, 0);
 
-  options.map((opt) => {
-    const str = input.replace("?", opt);
-    if (isValid(str, check)) arrangements++;
-    else arrangements += bar([str, check]);
-  });
-  return arrangements;
-}
+// part 2
+const part2 = records
+  .map((record) => {
+    let [springs, check] = record.split(" ");
+    let checks = check.split(",").map(Number);
 
-const sum = records.reduce((acc, record) => {
-  return acc + bar(record);
-}, 0);
-console.log("🚀 ~ sum:", sum);
+    // Repeat the checks and springs 5 times
+    checks = checks.concat(checks, checks, checks, checks);
+    springs = Array(5).fill(springs).join("?");
+
+    return count(springs, checks);
+  })
+  .reduce((a, b) => a + b, 0);
+
+console.log("🚀 ~ part1:", part1);
+console.log("🚀 ~ part2:", part2);
